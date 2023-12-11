2a1667179211822a4054f7f72c08f3db08216c2e
shubhamxg <shubham1316g@gmail.com>
2023-12-11 19:58:08 +0530

    recipe data model

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Recipe struct {
-	Name         string   `json:"name"`
-	Tags         []string `json:"tags"`
-	Ingredients  []string `json:"ingredients"`
-	Instructions []string `json:"instructions"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func main() {
