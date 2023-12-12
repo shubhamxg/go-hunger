72dd5826bdba4046bbd35f4b00f7d18760f7e133
shubhamxg <shubham1316g@gmail.com>
2023-12-12 07:50:14 +0530

    idk

--- main.go
+++ main.go
@@ -1,47 +1,78 @@
 package main
 
 import (
-	"net/http"
-	"time"
+	"encoding/json"
+	"fmt"
+	// "net/http"
+	"os"
+	// "time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/rs/xid"
+	// "github.com/rs/xid"
 )
 
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	Recipe RecipeData `json:"recipe"`
 }
 
-var recipes []Recipe
+type RecipeData struct {
+	Name             string            `json:"name"`
+	ID               string            `json:"id"`
+	Description      string            `json:"description"`
+	Tags             []string          `json:"tag"`
+	Ingredients      []Ingredient      `json:"ingredient"`
+	IngredientGroups []IngredientGroup `json:"ingredientGroup"`
+	Steps            []Step            `json:"step"`
+}
+
+type Ingredient struct {
+	Name        string `json:"name"`
+	Amount      string `json:"amount,omitempty"`
+	Unit        string `json:"unit,omitempty"`
+	Preparation string `json:"preparation,omitempty"`
+}
+
+type IngredientGroup struct {
+	Name        string       `json:"name"`
+	Ingredients []Ingredient `json:"ingredient"`
+}
+
+type Step struct {
+	Description string `json:"description"`
+}
+
+// var recipes []Recipe
 
 func init() {
-	recipes = make([]Recipe, 0)
+	// recipes = make([]Recipe, 0)
+	file, _ := os.ReadFile("recipes.json")
+	// fmt.Println(string(file))
+	if err := json.Unmarshal([]byte(file), &Recipe); err != nil {
+		fmt.Println("Error >", err)
+	}
 }
 
-func NewRecipeHandler(c *gin.Context) {
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	recipe.ID = xid.New().String()
-	recipe.PublishedAt = time.Now()
-	recipes = append(recipes, recipe)
-	c.JSON(http.StatusOK, recipe)
-}
+// func NewRecipeHandler(c *gin.Context) {
+// 	var recipe Recipe
+// 	if err := c.ShouldBindJSON(&recipe); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": err.Error(),
+// 		})
+// 		return
+// 	}
+// 	recipe.ID = xid.New().String()
+// 	recipe.PublishedAt = time.Now()
+// 	recipes = append(recipes, recipe)
+// 	c.JSON(http.StatusOK, recipe)
+// }
+
+// func ListRecipeHandler(c *gin.Context) {
+// 	c.JSON(http.StatusOK, recipes)
+// }
 
 func main() {
 	router := gin.Default()
-	router.POST("/recipes", NewRecipeHandler)
-	router.GET("/recipes", func(c *gin.Context) {
-		c.JSON(http.StatusOK, recipes)
-	})
+	// router.POST("/recipes", NewRecipeHandler)
+	// router.GET("/recipes", ListRecipeHandler)
 	router.Run()
 }
